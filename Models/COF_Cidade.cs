c13b8ae8ea6ba0855f0d45549aa2e04c8105c7f7
Adilson Resende <adilsonresendetp@gmail.com>
2021-04-25 22:19:26 -0300

    Implementado service COF_cidade

--- Models/COF_Cidade.cs
+++ Models/COF_Cidade.cs
@@ -3,6 +3,7 @@ namespace ENPS.Models
     public class COF_cidade
     {
         public int Id { get; set; }
+        public COF_estado COF_estado { get; set; }
         public bool Ativo { get; set; } = true;
         public string Nome { get; set; }
     }
--- Models/COF_Pais.cs
+++ Models/COF_Pais.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace ENPS.Models
 {
     public class COF_pais
@@ -5,5 +7,6 @@ namespace ENPS.Models
         public int Id { get; set; }
         public bool Ativo { get; set; } = true;
         public string Nome { get; set; }
+        public List<COF_estado> COF_estado { get; set; }
     }
 }
