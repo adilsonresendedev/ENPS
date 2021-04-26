8d3a2ff2dc58cf0543dc09c75c88450474446edb
Adilson Resende <adilsonresendetp@gmail.com>
2021-04-25 22:29:09 -0300

    Implementado COF_estadoService

--- Repos/COF_estadoRepos/COF_estadoRepo.cs
+++ Repos/COF_estadoRepos/COF_estadoRepo.cs
@@ -1,7 +1,15 @@
+using ENPS.Data;
+using ENPS.Models;
+using ENPS.Repos.BaseRepos;
+
 namespace ENPS.Repositorios.COF_estadoRepos
 {
-    public class COF_estadoRepo
+    public class COF_estadoRepo : BaseRepo<COF_estado>, ICOF_estadoRepo
     {
-        
+        private readonly DataContext _dataContext;
+        public COF_estadoRepo(DataContext dataContext) : base(dataContext)
+        {
+            _dataContext = dataContext;
+        }
     }
 }
